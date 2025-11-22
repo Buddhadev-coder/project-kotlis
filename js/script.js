3ceea5f67bb3d8821a6597b058e2ba25d163dfd9
Buddhadev-coder <sahoobuddhadev696@gmail.com>
2025-11-22 20:29:20 +0530

    this is second push

--- js/script.js
+++ js/script.js
@@ -38,10 +38,42 @@ btn_3.addEventListener("click", () => {
 
 
 });
+const submenuBtns = document.querySelectorAll(".mobile_menu_btn");
+
+submenuBtns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+
+        // ✔ Run submenu only on mobile
+         
+
+        e.preventDefault(); // Stop <a> jump
+
+        const submenu = btn.nextElementSibling;
+
+        if (!submenu.classList.contains("open")) {
+            submenu.classList.remove("hidden");
+            submenu.classList.add("open");
+            submenu.style.height = submenu.scrollHeight + "px";
+        }
+        else {
+            submenu.style.height = submenu.scrollHeight + "px";
+            setTimeout(() => submenu.style.height = "0px");
+            submenu.classList.remove("open");
+
+            submenu.addEventListener(
+                "transitionend",
+                () => submenu.classList.add("hidden"),
+                { once: true }
+            );
+        }
+    });
+});
+
 
 
 var swiper = new Swiper(".mySwiper", {
     speed: 800,
+    slidesPerView: 1,
     spaceBetween: 10,
     slidesPerView: 3,
     resistanceRatio: 0.6,
@@ -54,9 +86,59 @@ var swiper = new Swiper(".mySwiper", {
 
     },
     mousewheel: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-        type: "fraction",
-    },
+    breakpoints: {
+        320: {     // Extra small mobile
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        480: {     // Small mobile
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        576: {     // Large mobile
+            slidesPerView: 1,
+            spaceBetween: 15,
+        },
+        640: {     // Tablets portrait
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {     // Tablets landscape
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        992: {     // Small laptop
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        1024: {    // Laptop
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1280: {    // Desktop
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        1440: {    // Large desktop
+            slidesPerView: 3,
+            spaceBetween: 10,
+        }
+    }
+
+});
+
+document.getElementById("nextBtn").addEventListener("click", () => {
+    swiper.slideNext();
+});
+document.getElementById("prevBtn").addEventListener("click", () => {
+    swiper.slidePrev();
+});
+
+const lightbox = GLightbox({
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: true,
+    selector: "*[data-gallery]",
+    zoomable: false,
+    height: "auto",
 });
